4a23232c568bb87040e9125b20c222039a1fd2c1
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2021-05-01 18:55:38 +0300

    Delete js directory

--- js/menu.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.header__burger').click(function() {
-        $('.header__burger').toggleClass('active');
-        $('.header__navigation-list').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-});
